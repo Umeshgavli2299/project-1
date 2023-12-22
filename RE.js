a7c487ef35908e3b29a2c68a9bc8d74a5bd00ed6
Umeshgavli2299 <154583176+Umeshgavli2299@users.noreply.github.com>
2023-12-22 12:39:54 +0530

    Add files via upload

---
+++ RE.js
@@ -0,0 +1,132 @@
+function a()
+{
+    document.getElementById("kk").src="./Images/1continental-gt-home-motorcycle1.avif"
+}
+
+function b()
+{
+    document.getElementById("kk").src="./Images/2super-meteor-650.avif"
+}
+
+function c()
+{
+    document.getElementById("kk").src="./Images/3hunter-350-motorcycle-home.avif"
+}
+
+function d()
+{
+    document.getElementById("kk").src="./Images/4royal-enfield-scram-411-home-motorcycle.avif"
+}
+
+function e()
+{
+    document.getElementById("kk").src="./Images/5classic-350-motorcycle-section.avif"
+}
+
+function f()
+{
+    document.getElementById("kk").src="./Images/6royal-enfield-meteor-new-banner.avif"
+}
+
+function g()
+{
+    document.getElementById("kk").src="./Images/7sleethblack-banner-des.avif"
+}
+
+function h()
+{
+    document.getElementById("kk").src="./Images/8banner.avif"
+}
+
+function i()
+{
+    document.getElementById("kk").src="./Images/interceptor-home-motorcycle.avif"
+}
+
+function aa()
+{
+    document.getElementById("jj").src="./Images/himalayan-odyssey-2023.avif"
+}
+
+function ab()
+{
+    document.getElementById("jj").src="./Images/10.avif"
+}
+
+function ac()
+{
+    document.getElementById("jj").src="./Images/11.avif"
+}
+
+function ad()
+{
+    document.getElementById("jj").src="./Images/12.avif"
+}
+
+function ae()
+{
+    document.getElementById("jj").src="./Images/13.avif"
+}
+
+function af()
+{
+    document.getElementById("jj").src="./Images/14.avif"
+}
+
+function ba()
+{
+    document.getElementById("nn").src="./Images/art-of-motorcycling-apparel.avif"
+}
+
+function bb()
+{
+    document.getElementById("nn").src="./Images/15.avif"
+}
+
+function bc()
+{
+    document.getElementById("nn").src="./Images/16.avif"
+}
+function bd()
+{
+    document.getElementById("nn").src="./Images/17.avif"
+}
+
+function be()
+{
+    document.getElementById("nn").src="./Images/18.avif"
+}
+
+function bf()
+{
+    document.getElementById("nn").src="./Images/19.avif"
+}
+
+function ca()
+{
+    document.getElementById("dd").src="./Images/gma-home.avif"
+}
+
+function cb()
+{
+    document.getElementById("dd").src="./Images/20.avif"
+}
+
+function da()
+{
+    document.getElementById("ss").src="./Images/stores.avif"
+}
+
+function db()
+{
+    document.getElementById("ss").src="./Images/21.avif"
+}
+
+function dc()
+{
+    document.getElementById("ss").src="./Images/22.avif"
+}
+
+
+
+
